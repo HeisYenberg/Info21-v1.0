e351e3589be2293b5e7d868a141d43ac3335b9ac
bridges123 <i.s.andreevmail.rut@gmail.com>
2023-09-24 23:18:14 +0300

    full part4 v1

--- src/part4.sql
+++ src/part4.sql
@@ -42,7 +42,7 @@ CREATE OR REPLACE FUNCTION fnc_get_function_params(func_name varchar) RETURNS va
 $$
 WITH RECURSIVE params AS
                    (SELECT ip.specific_name,
-                           (ip.specific_name || ': ' || ip.parameter_name) AS param
+                           ip.parameter_name::varchar AS param
                     FROM information_schema.parameters ip
                     WHERE ip.specific_name = func_name
                     UNION
@@ -64,15 +64,16 @@ $$ language sql;
 CREATE OR REPLACE PROCEDURE fnc_get_funcs_with_params(OUT func_count integer) AS
 $$
 DECLARE
-    func varchar;
+    func record;
 BEGIN
     func_count := 0;
-    FOR func in (SELECT fnc_get_function_params('' || specific_name)
+    FOR func in (SELECT routine_name, fnc_get_function_params(specific_name::varchar) as params
                  FROM information_schema.routines
-                 WHERE specific_schema = 'public')
+                 WHERE routine_type = 'FUNCTION'
+                   AND specific_schema = 'public')
         LOOP
             IF func IS NOT NULL THEN
-                RAISE NOTICE '%', func;
+                RAISE NOTICE 'Name: %, Params: %', func.routine_name, func.params;
             END IF;
             func_count = func_count + 1;
         END LOOP;
@@ -97,6 +98,12 @@ CREATE OR REPLACE TRIGGER trg_some_trigger
     FOR EACH ROW
 EXECUTE PROCEDURE fnc_trigger_function();
 
+CREATE OR REPLACE TRIGGER trg_another_trigger
+    AFTER DELETE OR UPDATE
+    ON super_table
+    FOR EACH ROW
+EXECUTE PROCEDURE fnc_trigger_function();
+
 -- 3) Создать хранимую процедуру с выходным параметром, которая уничтожает все SQL DML триггеры
 -- в текущей базе данных. Выходной параметр возвращает количество уничтоженных триггеров.
 CREATE OR REPLACE PROCEDURE fnc_drop_all_triggers(OUT dropped_triggers integer) AS
@@ -118,3 +125,25 @@ END;
 $$ language plpgsql;
 
 CALL fnc_drop_all_triggers(0);
+
+
+-- 4) Создать хранимую процедуру с входным параметром, которая выводит имена и описания
+-- типа объектов (только хранимых процедур и скалярных функций),
+-- в тексте которых на языке SQL встречается строка, задаваемая параметром процедуры.
+CREATE OR REPLACE PROCEDURE fnc_print_objects_with_substr(IN substr varchar) AS
+$$
+DECLARE
+    row record;
+BEGIN
+    FOR row IN (SELECT routine_name, routine_type
+                FROM information_schema.routines
+                WHERE (routine_type = 'FUNCTION' OR routine_type = 'PROCEDURE')
+                  AND specific_schema = 'public'
+                  AND routine_definition ILIKE '%' || 'for' || '%')
+        LOOP
+            RAISE NOTICE 'Name: %, Type desc: %', row.routine_name, row.routine_type;
+        END LOOP;
+END;
+$$ language plpgsql;
+
+CALL fnc_print_objects_with_substr('spec');
